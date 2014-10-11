e51bd95371950b3c2f3d10b690ce337419decbcd
boardmanl <leslie+cloud9@araitanga.com>
2014-10-11 20:14:13 +0000

    Loadfirst round results and start coding the scoring and ranking part

--- app/scoring.js
+++ app/scoring.js
@@ -4,7 +4,7 @@
  * These functions all return a number which is the points the user has earned from that pick
  **/
  
-function soccerFixtureScoring(userPick, fixtureResult, scoringOptions){
+function fixtureScoring(userPick, fixtureResult, scoringOptions){
     //set fixture points to zero
     var totalPoints = 0;
     console.log("inSCORING");
@@ -28,10 +28,110 @@ function soccerFixtureScoring(userPick, fixtureResult, scoringOptions){
             }
         }
         
+        if ( scoringOption.type == "exactResult" ){
+            if (String(userPick.winner)==String(fixtureResult.winner)) { 
+                if ( (scoringOption.points - (Math.abs(userPick.homeScore - fixtureResult.homeScore)*scoringOption.lossMultiplier) 
+                                          - (Math.abs(userPick.awayScore - fixtureResult.awayScore)*scoringOption.lossMultiplier) ) >0 ) {
+                    totalPoints += scoringOption.points - (Math.abs(userPick.homeScore - fixtureResult.homeScore)*scoringOption.lossMultiplier) 
+                                          - (Math.abs(userPick.awayScore - fixtureResult.awayScore)*scoringOption.lossMultiplier);
+                }
+                //console.log("USER: %s, FIXTURE: %s, GD so +3 points:%s", userPick.scoreDifference, fixtureResult.scoreDifference,totalPoints);
+            }
+        }
+        
     });
     
     return totalPoints;
 }
+exports.FixtureScoring = fixtureScoring;
 
 
-exports.soccerFixtureScoring= soccerFixtureScoring;
+
+
+
+	// =====================================
+	// SCORING AND RANKING =================
+	// =====================================
+	// Scores a single fixture and updates the rankings for
+	// competition and round and fixture.
+
+function updateScoreByFixtureId(fixtureId){
+    var Fixture = require('../app/models/fixture');
+    var async = require('async');
+    Fixture.findById(fixtureId).exec(function(err, fixture){
+        if (err) console.log("fixture look up ERROR:"+err.toString());
+        else{
+            async.series([
+                scoreFixture(fixture),
+                // updateCompetitionFixtureRanking(fixture),
+                // updateCompetitionRoundRanking(fixture),
+                // updateCompetitionEventRanking(fixture),
+            ], function(err){
+                if (err){console.log('ERROR on scoreUpdate for fixture: %s',fixtureId)}
+            }); 
+        }
+    });
+}
+
+function scoreFixture(fixture){
+    /**
+     * Takes a fixture object and calcuates the score for each user who has a pick for that fixture.
+     **/
+    var PickFixture = require('../app/models/soccerPickFixture');
+    var Point = require('../app/models/point');
+    PickFixture.find({fixture:fixture._id}).exec(function(err,picks){
+       //console.log('PICKS\n%s',picks);
+       picks.forEach(function(userPick){
+            console.log("PICKS\n%s",userPick);
+            console.log('FIXTURE\nfixture:%s', fixture);
+            console.log('\n=====================\n');
+            // calc winning score and update db if score is not zero
+
+            var pickPoints = fixtureScoring(userPick, fixture, [{type:"winner", points:1},{type:"exactResult", points:1, lossMultiplier:1}]);
+            console.log('points = %s', pickPoints);
+            
+
+/**
+            Point.update({
+                type: 'fixture',
+                user: userPick.user,
+                competition: userPick.competition,
+                event: fixture.event,
+                round: fixture.round,
+                fixture: fixture._id},
+                {$set: {points: pickPoints}},
+                {upsert: true},
+                function(err){
+                    if (err) console.log("ERROR:"+err.toString());
+                    else {
+                        console.log("FIXTURE SCORED");
+                    }
+                }
+            );
+**/
+    
+       });
+    });
+}
+
+
+	// =====================================
+	// EXECUTABLE PART FOR TESTING =========
+	// =====================================
+	// Scores a single fixture and updates the rankings for
+	// competition and round and fixture.
+
+// connect to the database
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://golog:gogogadget@kahana.mongohq.com:10088/tipping2');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', function callback(){
+
+   // TEST UPDATE SCORE
+   updateScoreByFixtureId('542c9ae12367c9209a739150');
+
+    console.log("done");
+    
+});
