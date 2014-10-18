0e01fd9a8349dbeab8c17597cdbb66451e1e1720
boardmanl <leslie+cloud9@araitanga.com>
2014-10-18 03:34:32 +0000

    Fixed the updateuserpick script to add the round.

--- app/utilityScripts/updateUserPicks.js
+++ app/utilityScripts/updateUserPicks.js
@@ -2,17 +2,18 @@ var mongoose = require('mongoose');
 var User = require('../models/user');
 var Competition = require('../models/competition');
 var Team = require('../models/team');
+var Round = require('../models/round');
 var FixturePick = require('../models/fixturePick');
 
 function updatePicks(user_email, comp_name){
     var picks = [
-        // ROUND 1 Picks
         {user: user_email, 
         competition: comp_name, 
         fixture: '5434a4cc2367c9209a73916e', // CCM WPX
         winner: 'CCM',
         homeScore: 2,
-        awayScore: 0
+        awayScore: 0,
+        roundName: 'Round 2'
         },
 
         {user: user_email, 
@@ -20,7 +21,8 @@ function updatePicks(user_email, comp_name){
         fixture: '5434a4cc2367c9209a73916f', // MCY NUJ
         winner: 'MCY',
         homeScore: 3,
-        awayScore: 2
+        awayScore: 2,
+        roundName: 'Round 2'
         },
 
         {user: user_email, 
@@ -28,7 +30,8 @@ function updatePicks(user_email, comp_name){
         fixture: '5434a4cc2367c9209a739170', //ADU MBV
         winner: 'MBV',
         homeScore: 0,
-        awayScore: 2
+        awayScore: 2,
+        roundName: 'Round 2'
         },
 
         {user: user_email, 
@@ -36,7 +39,8 @@ function updatePicks(user_email, comp_name){
         fixture: '5434a4cc2367c9209a739171', // PTH BBR
         winner: 'BBR',
         homeScore: 1,
-        awayScore: 3
+        awayScore: 3,
+        roundName: 'Round 2'
 
         },
 
@@ -45,7 +49,8 @@ function updatePicks(user_email, comp_name){
         fixture: '5434a4cc2367c9209a739172', // SFC WSW
         winner: 'WSW',
         homeScore: 1,
-        awayScore: 2
+        awayScore: 2,
+        roundName: 'Round 2'
         }
     ];
     
@@ -54,24 +59,24 @@ function updatePicks(user_email, comp_name){
             if (err) console.log("ERROR:"+err.toString());
             else{
                 Competition.findOne({'name': comp_name}, function (err, competition) {
-                if (err) console.log("ERROR:"+err.toString());
-                else{
-    
+                    if (err) console.log("ERROR:"+err.toString());
+                    else{
                         Team.findOne({'shtcode': pick.winner}, function (err, winner) {
                             if (err) console.log("ERROR:"+err.toString());
                             else{
-                                console.log('WINNER %s', winner.name);
-                                FixturePick.update({
-                                    user: user._id, 
-                                    competition: competition._id, 
-                                    fixture: pick.fixture}, 
-                                    {$set: {winner: winner._id, homeScore: pick.homeScore, awayScore:pick.awayScore}},
-                                    {upsert: true},
-                                    function(err){
-                                        if (err) console.log("ERROR2:"+err.toString());
-                                    }
-                                );
-
+                                Round.findOne({competition:competition, name:pick.roundName}).exec(function(err,round){
+                                    console.log('WINNER %s', winner.name);
+                                    FixturePick.update({
+                                        user: user._id, 
+                                        competition: competition._id, 
+                                        fixture: pick.fixture}, 
+                                        {$set: {round:round.id, winner: winner._id, homeScore: pick.homeScore, awayScore:pick.awayScore}},
+                                        {upsert: true},
+                                        function(err){
+                                            if (err) console.log("ERROR2:"+err.toString());
+                                        }
+                                    );
+                                });
                             }
                         });
                     }
