88eee96e236230734ad32b150dc31885e6a0042d
boardmanl <leslie+cloud9@araitanga.com>
2015-01-08 09:32:19 +0000

    Adding Super Rugby

--- app/utilityScripts/SuperRugbyScripts.js
+++ app/utilityScripts/SuperRugbyScripts.js
@@ -18,7 +18,7 @@ function convertTime(time,inZone,outZone){
 
 //update League
 function updateLeagues(){
-    var League = require('./app/models/league');
+    var League = require('../models/league');
     League.update({name:'Super Rugby'},
         {$set:{
             sponsor:'Investec'
@@ -32,8 +32,8 @@ function updateLeagues(){
 
 //update Event
 function updateEvent(){
-    var Event = require('./app/models/event');
-    var League = require('./app/models/league');
+    var Event = require('../models/event');
+    var League = require('../models/league');
     League.findOne({name:"Super Rugby"}, function (err, league) {
         if (err) console.log("ERROR:"+err.toString());
         else {
@@ -48,11 +48,53 @@ function updateEvent(){
     });
 }
 
+function loadCompetitions(){
+    var Event = require('../models/event');
+    var League = require('../models/league');
+    var Competition = require('../models/competition');
+    var User = require('../models/user');
+
+    User.findOne({'local.email': "darcy@araitanga.com"}, function (err, user) {
+        if (err) console.log("ERROR:"+err.toString());
+        else{
+            
+            User.findOne({'local.email': "leslie@araitanga.com"}, function (err, user2) {
+                if (err) console.log("ERROR:"+err.toString());
+                else{
+            
+                    League.findOne({'name': "Super Rugby"}, function (err, league) {
+                        if (err) console.log("ERROR:"+err.toString());
+                        else{
+                            
+                            Event.findOne({'name': "2015 Season"}, function (err, event) {
+                                if (err) console.log("ERROR:"+err.toString());
+                                else{
+                            
+                                    Competition.create({
+                                        name: 'Wellington City Council',
+                                        usersAccepted: [user2.id],
+                                        league: league._id,
+                                        event: event._id
+                                    });
+                                }
+                            });
+                            
+                        }
+                    });
+                }
+            });
+            
+        }
+        
+    });
+}
+
+
 // update TEAMS
 function updateTeams(){
     console.log('updating teams');
-    var Team = require('./app/models/team');    
-    var League = require('./app/models/league');    
+    var Team = require('../models/team');    
+    var League = require('../models/league');    
 
     League.findOne({name:"Super Rugby"}, function (err, league) {
         Team.update({name: 'Brumbies'},{$set: {shtname:'Brumbies', shtcode:'BRM',league:league}},{upsert:true}, function(err){if (err) console.log("Team update Error:"+err.toString())});
@@ -81,9 +123,24 @@ function updateRounds(leagueName, eventName){
     console.log('updating rounds');
     var roundList = [
         {name:'Round 1',type: 'fixture', roundPosition: 1, firstFixture: "2015-02-13 19:35", lastFixture: "2015-02-16 18:05", ffTimeZone: "Pacific/Auckland"},
+        {name:'Round 2',type: 'fixture', roundPosition: 2, firstFixture: "2015-02-20 19:35", lastFixture: "2015-02-22 06:10", ffTimeZone: "Pacific/Auckland"},
+        {name:'Round 3',type: 'fixture', roundPosition: 3, firstFixture: "2015-02-27 19:35", lastFixture: "2015-03-01 06:10", ffTimeZone: "Pacific/Auckland"},
+        {name:'Round 4',type: 'fixture', roundPosition: 4, firstFixture: "2015-03-06 19:35", lastFixture: "2015-03-08 06:10", ffTimeZone: "Pacific/Auckland"},
+        {name:'Round 5',type: 'fixture', roundPosition: 5, firstFixture: "2015-03-13 19:35", lastFixture: "2015-03-15 04:05", ffTimeZone: "Pacific/Auckland"},
+        {name:'Round 6',type: 'fixture', roundPosition: 6, firstFixture: "2015-03-20 19:35", lastFixture: "2015-03-22 18:05", ffTimeZone: "Pacific/Auckland"},
+        {name:'Round 7',type: 'fixture', roundPosition: 7, firstFixture: "2015-03-27 19:35", lastFixture: "2015-03-29 06:10", ffTimeZone: "Pacific/Auckland"},
+        {name:'Round 8',type: 'fixture', roundPosition: 8, firstFixture: "2015-04-03 19:35", lastFixture: "2015-04-05 03:05", ffTimeZone: "Pacific/Auckland"},
+        {name:'Round 9',type: 'fixture', roundPosition: 9, firstFixture: "2015-04-10 19:35", lastFixture: "2015-04-12 03:05", ffTimeZone: "Pacific/Auckland"},
+        {name:'Round 10',type: 'fixture', roundPosition: 10, firstFixture: "2015-04-17 19:35", lastFixture: "2015-04-19 05:10", ffTimeZone: "Pacific/Auckland"},
+        {name:'Round 11',type: 'fixture', roundPosition: 11, firstFixture: "2015-04-24 19:35", lastFixture: "2015-04-26 18:05", ffTimeZone: "Pacific/Auckland"},
+        {name:'Round 12',type: 'fixture', roundPosition: 12, firstFixture: "2015-05-01 19:35", lastFixture: "2015-05-03 05:10", ffTimeZone: "Pacific/Auckland"},
+        {name:'Round 13',type: 'fixture', roundPosition: 13, firstFixture: "2015-05-08 19:35", lastFixture: "2015-05-10 01:00", ffTimeZone: "Pacific/Auckland"},
+        {name:'Round 14',type: 'fixture', roundPosition: 14, firstFixture: "2015-05-15 19:35", lastFixture: "2015-05-17 05:10", ffTimeZone: "Pacific/Auckland"},
+        {name:'Round 15',type: 'fixture', roundPosition: 15, firstFixture: "2015-05-22 19:35", lastFixture: "2015-05-24 05:10", ffTimeZone: "Pacific/Auckland"},
+        {name:'Round 16',type: 'fixture', roundPosition: 16, firstFixture: "2015-05-29 19:35", lastFixture: "2015-05-31 05:10", ffTimeZone: "Pacific/Auckland"},
+        {name:'Round 17',type: 'fixture', roundPosition: 17, firstFixture: "2015-06-05 19:35", lastFixture: "2015-06-07 03:05", ffTimeZone: "Pacific/Auckland"},
+        {name:'Round 18',type: 'fixture', roundPosition: 18, firstFixture: "2015-06-12 19:35", lastFixture: "2015-06-14 05:10", ffTimeZone: "Pacific/Auckland"},
     ];
-    
-    NEED TO EDIT BELWO TP ADD LASTDFIX DATE TO DATABASE
     
     League.findOne({name:leagueName}, function (err, league) {
         if (err) console.log("ERROR:"+err.toString());
@@ -96,7 +153,7 @@ function updateRounds(leagueName, eventName){
                         Round.update({name: roundData.name,
                             league: league._id,
                             event: event._id},
-                            {$set: {roundPosition: roundData.roundPosition,closeDate:(convertTime(roundData.firstFixture, roundData.ffTimeZone,"UTC"))}},
+                            {$set: {roundPosition: roundData.roundPosition, type:roundData.type, closeDate:(convertTime(roundData.firstFixture, roundData.ffTimeZone,"UTC")),lastFixtureDate:(convertTime(roundData.lastFixture, roundData.ffTimeZone,"UTC"))}},
                             {upsert: true},
                             function(err){
                                 if (err) console.log("Round update Error:"+err.toString());                                
@@ -115,8 +172,10 @@ mongoose.connect('mongodb://golog:gogogadget@kahana.mongohq.com:10088/tipping2')
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
 db.once('open', function callback(){
-    updateLeagues();
-    //updateEvent();
+    //updateLeagues(); DONE
+    //updateEvent(); DONE 2015
+    loadCompetitions();
     //updateTeams();
+    //updateRounds('Super Rugby','2015 Season');
     console.log("done");
 });
